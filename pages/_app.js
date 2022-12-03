8d47ee109cb00e54ddd783cdd62e52487a4e3e96
moat1 <108268542+moat1@users.noreply.github.com>
2022-12-03 23:59:24 +0900

    create-next-appのデフォルト画面を白紙化

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <div>
+    <Head>
+      <title>Todo Application</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    <Component {...pageProps} />
+  </div>;
 }
 
-export default MyApp
+export default MyApp;
