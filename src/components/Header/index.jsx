f24aacd01a3c626a55879b8e202472c7a834e7bf
moat1 <108268542+moat1@users.noreply.github.com>
2022-12-06 16:07:26 +0900

    Headerの改修

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,14 @@
 import classes from '/src/components/Header/Header.module.css';
 
 export function Header() {
-  return <header className={classes.header}>Header</header>;
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.title}>Todo App</h1>
+      <ul className={classes.links}>
+        <li className={classes.link}>Top</li>
+        <li className={classes.link}>About</li>
+        <li className={classes.link}> Contact</li>
+      </ul>
+    </header>
+  );
 }
