e32f65228905437bfbfaa024bc207aaae875badd
moat1 <108268542+moat1@users.noreply.github.com>
2022-12-08 02:29:43 +0900

    ヘッダーの修正（グローバルナビのカレント表示・CurrentPageの削除・デザインの微修正）

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,37 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import classes from '/src/components/Header/Header.module.css';
 
 export function Header() {
+  const router = useRouter();
+
   return (
     <header className={classes.header}>
       <h1 className={classes.title}>Todo App</h1>
       <ul className={classes.links}>
-        <li className={classes.link}>
+        <li
+          className={`${classes.link} ${
+            router.pathname.endsWith('/') ? classes.current : classes.noncurrent
+          }`}
+        >
           <Link href='/'>Top</Link>
         </li>
-        <li className={classes.link}>
+        <li
+          className={`${classes.link} ${
+            router.pathname.endsWith('/about')
+              ? classes.current
+              : classes.noncurrent
+          }`}
+        >
           <Link href='/about'>About</Link>
         </li>
-        <li className={classes.link}>
+        <li
+          className={`${classes.link} ${
+            router.pathname.endsWith('/contact')
+              ? classes.current
+              : classes.noncurrent
+          }`}
+        >
           <Link href='/contact'>Contact</Link>
         </li>
       </ul>
