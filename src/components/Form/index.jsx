d96da786615cb43ab694e0cc1dd4ddc27a586659
moat1 <108268542+moat1@users.noreply.github.com>
2022-12-11 22:26:58 +0900

    material uiのインストール、フォーム・新着順の追加

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,23 +1,52 @@
+import { userAgent } from 'next/server';
+import { useRef } from 'react';
 import classes from '/src/components/Form/Form.module.css';
+import CreateIcon from '@mui/icons-material/Create';
 
 export function Form() {
+  const todoTitle = useRef(null);
+  const todoContents = useRef(null);
+
+  const addBtn = (e) => {
+    e.preventDefault();
+
+    console.log(todoTitle.current.value);
+    console.log(todoContents.current.value);
+    alert(
+      'タイトル : ' +
+        todoTitle.current.value +
+        '　' +
+        'テキスト : ' +
+        todoContents.current.value +
+        '　' +
+        'でよろしいですか。'
+    );
+    todoTitle.current.value = '';
+    todoContents.current.value = '';
+  };
+
   return (
     <form className={classes.create_new_post}>
-      <h3>/ 新規投稿 /</h3>
+      <h3>
+        <CreateIcon className={classes.create} />
+        新規投稿
+      </h3>
       <p>タイトル</p>
       <input
         className={classes.titleInput}
         type='text'
         placeholder='タイトルを入力'
+        ref={todoTitle}
       />
       <p>テキスト</p>
       <textarea
         className={classes.textInput}
         type='text'
         placeholder='todoを入力'
+        ref={todoContents}
       ></textarea>
       <br></br>
-      <button>+ Add</button>
+      <button onClick={addBtn}>+ Add</button>
     </form>
   );
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,29 @@
-import { CreateNewPostBtn } from '/src/components/CreateNewPostBtn';
+import Link from 'next/link';
 import classes from '/src/components/Sidebar/Sidebar.module.css';
+import AddIcon from '@mui/icons-material/Add';
+import ViewListIcon from '@mui/icons-material/ViewList';
 
 export function Sidebar() {
   return (
     <div className={classes.sidebar}>
-      <CreateNewPostBtn />
+      <ul>
+        <li>
+          <Link href='/create_new_post'>
+            <p className={classes.createNewPost}>
+              <AddIcon className={classes.createIcon} />
+              新規作成
+            </p>
+          </Link>
+        </li>
+        <li>
+          <Link href='/latest'>
+            <p className={classes.latest}>
+              <ViewListIcon className={classes.viewListIcon} />
+              新着順
+            </p>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
